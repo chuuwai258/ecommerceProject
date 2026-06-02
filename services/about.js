73412e06c2a840248dc1b7551d018b994b85ef11
chuuwai258 <chuuwaihnin@gmail.com>
2026-06-02 00:39:21 -0700

    ecommerce website

---
+++ services/about.js
@@ -0,0 +1,121 @@
+const customerReview=document.querySelector("#customer-review")
+async function getProduct(){
+  try{
+    const response=await fetch("https://fakestoreapi.com/products/2")
+    const data=await response.json()
+
+    customerReview.innerHTML=`
+    <div class="grid grid-cols-2 p-8">
+      <div>
+        <img src="${data.image}" class="h-[480px] w-[550px] object-contain bg-[#F9F1E7] rounded-[10px] p-8 ml-25">
+      </div>
+
+      <div class="mr-10 space-y-2">
+        <h1 class="text-3xl font-semibold">${data.title}</h1>
+        <p class="text-foreground-muted text-2xl">$${data.price}</p>
+
+        <div class="flex gap-3">
+          <div>
+            <span class="fa fa-star checked" style="color:gold;"></span>
+            <span class="fa fa-star checked" style="color:gold;"></span>
+            <span class="fa fa-star checked" style="color:gold;"></span>
+            <span class="fa fa-star checked" style="color:gold;"></span>
+            <span class="fa fa-star-half checked" style="color:gold;"></span>
+          </div>
+          <img src="./images/Line 5.png" alt="line5" class="bg-[#F9F1E7]">
+          <h2 class="text-foreground-muted">5 Customer Review</h2>          
+        </div>
+
+        <p class="text-justify">${data.description}</p>        
+        <div class="space-y-2 mt-4">
+          <p>Size</p>
+          <div class="flex gap-3">
+            <p class="border bg-[#F9F1E7] rounded-[5px] w-[30px] h-[30px] text-center">L</p>
+            <p class="border bg-[#F9F1E7] rounded-[5px] w-[30px] h-[30px] text-center">XL</p>
+            <p class="border bg-[#F9F1E7] rounded-[5px] w-[30px] h-[30px] text-center">XS</p>
+          </div>
+        </div>
+
+        <div class="space-y-2 mt-4">
+          <p>Color</p>
+          <div class="flex gap-3">
+            <button class="border bg-[#816DFA] rounded-full w-[30px] h-[30px] "><button>
+            <button class="border bg-[#000000] rounded-full w-[30px] h-[30px]"></button>
+            <button class="border bg-[#B88E2F] rounded-full w-[30px] h-[30px]"></button>
+          </div>
+        </div>
+
+        <div class="flex gap-6 mt-9 mb-7 ">
+          <button class="flex gap-5 border border-[#9F9F9F] px-4 py-2 rounded-[10px] ">
+          <span id="decrease">-</span>
+          <span id="zero">0</span>
+          <span id="increase">+</span>
+          </button>
+          <button class="px-6 py-2 border rounded-[10px]">Add to Cart</button>
+          <button class="px-6 py-2 border rounded-[10px]"> + Compare</button>          
+        </div>
+
+        <div class="flex gap-5  border-t border-slate-300 pt-10">
+          <div class="text-foreground-muted">
+            <p>SKU<span class="pl-14">:</span></p>
+            <p>Category<span class="pl-3">:</span></p>
+            <p>Tags<span class="pl-12">:</span></p>
+            <p>Share<span class="pl-10">:</span></p>
+          </div>
+
+          <div>
+            <div class="text-foreground-muted">
+              <p>TS001</p>
+              <p>Men's Clothing</p>
+              <p>T-Shirt, Casual, Fashion, Cotton</p>
+            </div>
+            <div class="flex gap-3 mt-1 font-bold">
+              <a href="#"><i class="fa-brands fa-facebook"></i></a>
+              <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+              <a href="#"><i class="fa-brands fa-square-twitter"></i></a>
+            </div>
+          </div>        
+        </div> 
+      </div> 
+    </div>      
+    `
+    const increaseBtn=document.querySelector("#increase")
+    const decreaseBtn=document.querySelector("#decrease")
+    const zero=document.querySelector("#zero")
+
+    let count=0
+    increaseBtn.addEventListener("click",()=>{
+      if(count<5){
+        count++
+        zero.textContent=count
+      }
+    })
+
+    decreaseBtn.addEventListener("click",()=>{
+      if(count>0){
+        count--
+        zero.textContent=count
+      }
+    })
+
+  }
+  catch(error){
+    console.log(error)
+  }
+}
+getProduct()
+
+
+// sidebar
+const cartBtn=document.querySelector("#cart-btn")
+const sideBar=document.querySelector("#sidebar")
+const closeBtn=document.querySelector("#close-btn")
+
+cartBtn.addEventListener("click",(e)=>{
+  e.preventDefault()
+  sideBar.classList.toggle("translate-x-full")
+})
+
+closeBtn.addEventListener("click",()=>{
+  sideBar.classList.add("translate-x-full")
+})
---
+++ services/products.js
@@ -0,0 +1,62 @@
+/** @format */
+const productCard=document.querySelector("#product-card")
+async function getProducts(){
+  try{
+    const response=await fetch("https://fakestoreapi.com/products")
+    const data=await response.json()
+    const product=data.map(item=>{
+      return `      
+        <div class="relative border border-slate-300 shadow-2xl p-6 rounded-[10px] overflow-hidden bg-white">
+          <div class="w-full h-[170px]  lg:h-[210px] md:h-[150px] mb-6 ">
+            <img src= ${item.image} class="h-[150px] w-full md:h-[150px] lg:h-[200px] md:w-full lg:w-full object-contain">
+          </div>
+      
+          <div class=" text-left md:leading-7 lg:leading-10 text-sans">
+            <h3 class="line-clamp-1 md:font-medium lg:font-semibold md:text-lg lg:text-xl ">${item.title}</h3>
+            <p class="lg:font-semibold text-foreground-muted ">${item.category}</p>
+            <h4 class="font-bold  lg:text-lg ">$${item.price}</h4>
+          </div>
+
+          <div class="absolute inset-0 bg-black/40 flex flex-col items-center justify-center gap-5 opacity-0 hover:opacity-100">
+            <button class="bg-white px-7 py-2 rounded-[10px] text-primary ">Add to cart</button>
+            <div class="flex gap-4 text-white">
+              <buttton class="flex items-center gap-3"><i class="fa-solid fa-share-nodes"></i>Share</button> 
+              <buttton class="flex items-center gap-3"><i class="fa-solid fa-code-compare"></i>Compare</button>
+              <buttton class="flex items-center gap-3"><i class="fa-regular fa-heart"></i>Like</button>
+            </div>
+          </div>        
+        </div>      
+  `
+})
+     
+    productCard.innerHTML=product.join("")
+  }catch(error){
+    console.log(error)
+  }
+  
+}
+export {getProducts}
+
+//sidebar
+const cartBtn=document.querySelector("#cart-btn")
+const sideBar=document.querySelector("#sidebar")
+const closeBtn=document.querySelector("#close-btn")
+
+cartBtn.addEventListener("click",(e)=>{
+  e.preventDefault()
+  sideBar.classList.toggle("translate-x-full")
+})
+
+closeBtn.addEventListener("click",()=>{
+  sideBar.classList.add("translate-x-full")
+})
+
+
+
+
+
+
+
+
+
+
